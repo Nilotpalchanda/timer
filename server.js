c0035944a1eb23dcf9276e1b149b210e102c37b6
Nilotpalchanda <nilotpalchanda14@gmail.com>
2018-02-08 02:15:16 +0530

    Project start add index.ejs

--- server.js
+++ server.js
@@ -11,8 +11,38 @@ var routes = require('./route/index.js');
 
 app.set('view engine','ejs');
 
+// add css and js
+
+app.use(express.static(__dirname + '/public/css'));
+app.use(express.static(__dirname + '/public/js'));
 
 
+app.use(express.static(__dirname + '/public/images'));
+app.use(express.static(__dirname + '/public/images/about'));
+app.use(express.static(__dirname + '/public/images/author'));
+app.use(express.static(__dirname + '/public/images/blog'));
+app.use(express.static(__dirname + '/public/images/clients'));
+app.use(express.static(__dirname + '/public/images/icons'));
+app.use(express.static(__dirname + '/public/images/portfolio'));
+app.use(express.static(__dirname + '/public/images/team'));
+
+app.use(express.static(__dirname + '/public/plugins/bootstrap'));
+app.use(express.static(__dirname + '/public/plugins/ionicons'));
+app.use(express.static(__dirname + '/public/plugins/animate-css'));
+app.use(express.static(__dirname + '/public/plugins/owl-carousel'));
+
+
+app.use(express.static(__dirname + '/public/plugins/facncybox'));
+app.use(express.static(__dirname + '/public/plugins/slider'));
+app.use(express.static(__dirname + '/public/plugins/animate-css'));
+app.use(express.static(__dirname + '/public/plugins/owl-carousel'));
+
+
+app.use(express.static(__dirname + '/public/plugins/jQurey'));
+app.use(express.static(__dirname + '/public/plugins/form-validation'));
+app.use(express.static(__dirname + '/public/plugins/wow-js'));
+app.use(express.static(__dirname + '/public/plugins/slider'));
+app.use(express.static(__dirname + '/public/plugins/owl-carousel'));
 
 // ================================================================
 // setup routes
